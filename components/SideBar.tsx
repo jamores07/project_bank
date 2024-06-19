df5ae71ffa845c766b15e518dca7b8cee4a62ad9
John Amores <kayak.stated_0b@icloud.com>
2024-06-19 12:19:49 -0400

    LeftSide bar structure completed --01:15:48

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,33 +1,57 @@
-import { sidebarLinks } from '@/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { sidebarLinks } from '@/constants';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 const SideBar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
+
   return (
     <section className='sidebar'>
         <nav className='flex flex-col gap-4'>
-            <Link className='mb-12 cursor-pointer items-center gap-2' href="/">
-                <Image 
-                    src="/icons/logo.svg"
-                    width={34}
-                    height={34}
-                    alt="Horizon Bank Logo"
-                    className='size-[24px] max-xl:size-14'
-                />
-                <h1 className="sidebar-logo">Horizon</h1>
-            </Link>
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image 
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Horizon</h1>
+        </Link>
 
-            {/* Links */}
-            {sidebarLinks.map((item) => {
-              const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
-              
-              return (
-                <></>
-              )
-            
-            })}
+        {/* Links */}
+        {sidebarLinks.map((item) => {
+          const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+
+          return (
+            <Link href={item.route} key={item.label}
+              className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
+            >
+              <div className="relative size-6">
+                <Image 
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    'brightness-[3] invert-0': isActive
+                  })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          )
+        })}
+
+        {/* User */}
         </nav>
+
+        {/* Footer */}
     </section>
   )
 }
